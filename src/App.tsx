8942cf983306b9ee680973814af1a0c6456b69a1
molang404 <art104233@naver.com>
2026-05-13 10:44:05 +0900

    ContextAPI를 통한 Theme 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
 import { RouterProvider } from "react-router";
 import GetRouter from "./router/GetRouter.tsx";
 import { ThemeProvider } from "styled-components";
-import { lightTheme } from "./styles/theme.ts";
+import { darkTheme, lightTheme } from "./styles/theme.ts";
 import { GlobalStyle } from "./styles/GlobalStyle.ts";
+import { useEffect, useState } from "react";
+import  { ThemeContext, type ThemeType } from "./contexts/theme/ThemeContext.ts";
 
 function App() {
+    const [ theme, setTheme ] = useState<ThemeType>(() => {
+        const savedTheme = localStorage.getItem("theme");
+        return savedTheme === "dark" ? "dark" : "light";
+    });
+
+    const onChangeTheme = () => {
+        setTheme(prev => prev === "light" ? "dark" : "light");
+    };
+
+    useEffect(() => {
+        localStorage.setItem("theme", theme);
+    }, [theme])
+
     return (
-        <ThemeProvider theme={lightTheme}>
-            <GlobalStyle />
-            <RouterProvider router={GetRouter}></RouterProvider>;
-        </ThemeProvider>
+        <ThemeContext.Provider value={{ theme, onChangeTheme }}>
+            <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+                <GlobalStyle />
+                <RouterProvider router={GetRouter}></RouterProvider>;
+            </ThemeProvider>
+        </ThemeContext.Provider>
     );
 }
 

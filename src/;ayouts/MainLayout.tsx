cddff95d5ed6889d47cb1915e21e46e389cb0d61
molang404 <art104233@naver.com>
2026-05-13 16:19:29 +0900

    복원

---
+++ src/;ayouts/MainLayout.tsx
@@ -0,0 +1,33 @@
+import { Outlet } from "react-router";
+import styled from "styled-components";
+import MainHeader from "../components/layout/MainHeader.tsx";
+import MainFooter from "../components/layout/MainFooter.tsx";
+
+const LayoutWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    min-height: 100dvh;
+`;
+
+const MainContainer = styled.main`
+    flex: 1;
+    width: 100%;
+    max-width: 1000px;
+    margin: 0 auto;
+    // 모바일 고려
+    padding: 40px 20px;
+`;
+
+function MainLayout() {
+    return (
+        <LayoutWrapper>
+            <MainHeader />
+            <MainContainer>
+                <Outlet />
+            </MainContainer>
+            <MainFooter />
+        </LayoutWrapper>
+    );
+}
+
+export default MainLayout;

76a14628d2a26242003f28d715f2ffb4e00447ac
molang404 <art104233@naver.com>
2026-05-20 09:50:18 +0900

    adminCategoryListPage API 작성 완료

---
+++ src/api/admin/adminCategoryApi.ts
@@ -0,0 +1,15 @@
+import axiosInstance from "../axiosInstance.ts";
+import type { Category } from "../../types/category.type.ts";
+
+const fetchCategoryList = async (): Promise<Category[]> => {
+    const response = await axiosInstance.get("/admin/category/list");
+    return response.data.date;
+};
+
+// 카테고리 등록 API
+
+// 카테고리 수정 API
+
+export default {
+    fetchCategoryList,
+};
--- src/pages/admin/category/AdminCategoryListPage.tsx
+++ src/pages/admin/category/AdminCategoryListPage.tsx
@@ -1,5 +1,27 @@
+import { useEffect, useState } from "react";
+import adminCategoryApi from "../../../api/admin/adminCategoryApi.ts";
+import type { Category } from "../../../types/category.type.ts";
+
 function AdminCategoryListPage() {
     // useEffect를 통해 role을 판단하면 초기 렌더링이 끝난 후에 컴포넌트 내에서(화면이 그려진 후) 판별이 이루어짐
+    const [categiries, setCategories] = useState<Category[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const loadCategories = async () => {
+            try {
+                const data = await adminCategoryApi.fetchCategoryList();
+                setCategories(data);
+            } catch (error) {
+                console.log(error);
+                alert("카테고리 목록을 불러오는데 실패했습니다.");
+            } finally {
+                setIsLoading(false);
+            }
+        }
+
+        loadCategories().then(() => {});
+    }, []);
 
     return <></>;
 }

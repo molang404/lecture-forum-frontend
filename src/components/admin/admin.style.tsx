c4a4e731d3405c8dda006d23a5ae8b46d7eef504
molang404 <art104233@naver.com>
2026-05-20 10:08:40 +0900

    toggleCategoryStatus 기능 추가, Badge 생성, adminCategoryCreatePage 생성, adminCategoryListPage 1차 작성

--- src/components/admin/admin.style.tsx
+++ src/components/admin/admin.style.tsx
@@ -1,31 +1,4 @@
-import axiosInstance from "../axiosInstance.ts";
-import type { Category } from "../../types/category.type.ts";
-import type { AdminCreateCategoryInputType } from "../../schemas/admin/category/adminCreateCategorySchema.ts";
-
-const fetchCategoryList = async (): Promise<Category[]> => {
-    const response = await axiosInstance.get("/admin/category/list");
-    return response.data.data;
-};
-
-// 카테고리 등록 API
-const createCategory = async (data: AdminCreateCategoryInputType): Promise<Category> => {
-    const response = await axiosInstance.post("/admin/category/create", data);
-    return response.data.data;
-};
-
-// 카테고리 수정 API
-
-// 카테고리 온오프 API
-const toggleCategoryStatus = async (id: number) => {
-    const response = await axiosInstance.patch(`/admin/category/${id}/status`);
-    return response.data.data;
-};
-
-export default {
-    fetchCategoryList,
-    createCategory,
-    toggleCategoryStatus,
-};import styled from "styled-components";
+import styled from "styled-components";
 
 export const AdminContainer = styled.div`
     display: flex;
@@ -98,5 +71,5 @@ export const AdminButtonGroup = styled.div<{ $align?: "left" | "right" | "center
     gap: 12px;
     align-items: center;
     justify-content: ${({ $align = "right" }) =>
-    $align === "right" ? "flex-end" : $align === "center" ? "center" : "flex-start"};
+        $align === "right" ? "flex-end" : $align === "center" ? "center" : "flex-start"};
 `;
--- src/pages/admin/category/AdminCategoryListPage.tsx
+++ src/pages/admin/category/AdminCategoryListPage.tsx
@@ -1,13 +1,32 @@
 import { useEffect, useState } from "react";
 import adminCategoryApi from "../../../api/admin/adminCategoryApi.ts";
-import type { Category } from "../../../types/category.type.ts";
+import { type Category, CategoryStatus } from "../../../types/category.type.ts";
+import Button from "../../../components/common/button/Button.tsx";
+import { Link } from "react-router";
+import Card from "../../../components/common/card/Card.tsx";
+import {
+    AdminContainer,
+    AdminLoadingText,
+    AdminPageHeader,
+    AdminTable,
+    AdminTableWrapper,
+    AdminTd,
+    AdminTh,
+    AdminTitle,
+} from "../../../components/admin/admin.style.tsx";
+import Badge from "../../../components/common/badge/Badge.tsx";
+import { FiRefreshCcw, FiTrash2 } from "react-icons/fi";
 
 function AdminCategoryListPage() {
-    // useEffect를 통해 role을 판단하면 초기 렌더링이 끝난 후에 컴포넌트 내에서(화면이 그려진 후) 판별이 이루어짐
-    const [categiries, setCategories] = useState<Category[]>([]);
+    const [categories, setCategories] = useState<Category[]>([]);
     const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
+        // useEffect 안에서 비동기함수를 async - await 방법으로 사용할거라면
+        // 함수를 만들어서 감싸주고, 그걸 실행하도록 문법에 맞춰 적음
+        // 그리고 그 함수 실행 역시 비동기함수에 대한 실행이기 때문에
+        // then(() => {})      아무것도 안하는 then을 붙여줌
+
         const loadCategories = async () => {
             try {
                 const data = await adminCategoryApi.fetchCategoryList();
@@ -18,12 +37,101 @@ function AdminCategoryListPage() {
             } finally {
                 setIsLoading(false);
             }
-        }
+        };
 
         loadCategories().then(() => {});
     }, []);
 
-    return <></>;
+    const handleToggleCategoryStatus = async (id: number) => {
+        // 백엔드에게 그 카테고리의 status를 바꿔줘 -> 함수 실행할 때 id를 받아야 함
+        try {
+            const result = await adminCategoryApi.toggleCategoryStatus(id);
+            alert(`카테고리가 성공적으로 ${result.status}로 변경되었습니다.`);
+
+            // 백엔드에게 목록을 요청하지 않고, 화면의 데이터만 교체해줄 것임
+            setCategories(prev =>
+                prev.map(item => (item.id === id ? { ...item, status: result.status } : item)),
+            );
+        } catch (error) {
+            console.log(error);
+            alert("카테고리 변경 중 오류가 발생되었습니다.");
+        }
+    };
+
+    return (
+        <AdminContainer>
+            <AdminPageHeader>
+                <AdminTitle>카테고리 관리</AdminTitle>
+                <Button
+                    color={"primary"}
+                    variant={"contained"}
+                    as={Link}
+                    to={"/admin/category/create"}>
+                    + 카테고리 추가
+                </Button>
+            </AdminPageHeader>
+
+            <Card>
+                {isLoading ? (
+                    <AdminLoadingText>불러오는 중...</AdminLoadingText>
+                ) : (
+                    <AdminTableWrapper>
+                        <AdminTable>
+                            <thead>
+                                <tr>
+                                    <AdminTh $width={"10%"}>ID</AdminTh>
+                                    <AdminTh $width={"65%"}>카테고리명</AdminTh>
+                                    <AdminTh $width={"15%"}>상태</AdminTh>
+                                    <AdminTh $width={"15%"}>관리</AdminTh>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {categories.length === 0 && (
+                                    <tr>
+                                        <AdminTd
+                                            colSpan={4}
+                                            style={{ textAlign: "center", padding: "100px" }}>
+                                            등록된 카테고리가 없습니다.
+                                        </AdminTd>
+                                    </tr>
+                                )}
+                                {categories.map(item => (
+                                    <tr key={item.id}>
+                                        <AdminTd>{item.id}</AdminTd>
+                                        <AdminTd>{item.name}</AdminTd>
+                                        <AdminTd>
+                                            <Badge
+                                                color={
+                                                    item.status === CategoryStatus.ACTIVE
+                                                        ? "success"
+                                                        : "secondary"
+                                                }>
+                                                {item.status === CategoryStatus.ACTIVE
+                                                    ? "활성"
+                                                    : "비활성"}
+                                            </Badge>
+                                        </AdminTd>
+                                        <AdminTd>
+                                            <Button
+                                                color={"primary"}
+                                                variant={"icon"}
+                                                onClick={() => handleToggleCategoryStatus(item.id)}>
+                                                {item.status === CategoryStatus.ACTIVE ? (
+                                                    <FiTrash2 size={18} />
+                                                ) : (
+                                                    <FiRefreshCcw size={18} />
+                                                )}
+                                            </Button>
+                                        </AdminTd>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </AdminTable>
+                    </AdminTableWrapper>
+                )}
+            </Card>
+        </AdminContainer>
+    );
 }
 
 export default AdminCategoryListPage;

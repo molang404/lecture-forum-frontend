a26ac29cfdb9f3064ab4462a4bfe3f7009e6ae34
molang404 <art104233@naver.com>
2026-05-20 16:23:10 +0900

    AdminCategoryEditPage 완성

--- src/api/admin/adminCategoryApi.ts
+++ src/api/admin/adminCategoryApi.ts
@@ -7,6 +7,11 @@ const fetchCategoryList = async (): Promise<Category[]> => {
     return response.data.data;
 };
 
+const fetchCategoryById = async (id: number) => {
+    const response = await axiosInstance.get(`/admin/category/${id}`);
+    return response.data.data;
+};
+
 // 카테고리 등록 API
 const createCategory = async (data: AdminCreateCategoryInputType): Promise<Category> => {
     const response = await axiosInstance.post("/admin/category/create", data);
@@ -14,6 +19,10 @@ const createCategory = async (data: AdminCreateCategoryInputType): Promise<Categ
 };
 
 // 카테고리 수정 API
+const updateCategory = async (id: number, data: AdminCreateCategoryInputType): Promise<Category> => {
+    const response = await axiosInstance.patch(`admin/category/${id}`, data);
+    return response.data.data;
+}
 
 // 카테고리 온오프 API
 const toggleCategoryStatus = async (id: number) => {
@@ -25,78 +34,6 @@ export default {
     fetchCategoryList,
     createCategory,
     toggleCategoryStatus,
-};import styled from "styled-components";
-
-export const AdminContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 24px;
-    width: 100%;
-`;
-
-export const AdminPageHeader = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 8px;
-`;
-
-export const AdminTitle = styled.h2`
-    font-size: 24px;
-    font-weight: 700;
-`;
-
-export const AdminLoadingText = styled.div`
-    text-align: center;
-    padding: 40px;
-    color: ${props => props.theme.colors.text.disabled};
-`;
-
-// PC에서는 상관 없는데, 모바일 때문에 한 번 테이블을 감싸는 것
-export const AdminTableWrapper = styled.div`
-    overflow-x: auto; // X축 방향으로 스크롤바를 허용하겠다
-`;
-
-export const AdminTable = styled.table`
-    width: 100%;
-    border-collapse: collapse;
-`;
-
-export const AdminTh = styled.th<{ $width?: string }>`
-    width: ${props => props.$width};
-    text-align: left;
-    padding: 12px 16px;
-    background-color: ${props => props.theme.colors.background.default};
-    color: ${props => props.theme.colors.text.disabled};
-    font-size: 13px;
-    font-weight: 600;
-    border-bottom: 2px solid ${props => props.theme.colors.divider};
-`;
-
-export const AdminTd = styled.td`
-    // td는 flex를 쓸 수 없음
-    // 그 안에 들어가는 요소에 대한 정렬은 text-align과 vertical-align을 통해서 해야 함
-    padding: 16px;
-    font-size: 14px;
-    border-bottom: 1px solid ${props => props.theme.colors.divider};
-    vertical-align: middle;
-`;
-
-export const AdminForm = styled.form`
-    display: flex;
-    flex-direction: column;
-    gap: 32px;
-`;
-
-// 매 번 styled-components의 props에 대한 함수를 props => 로 써줬었던 것은
-// 그렇게 만드는 함수의 매개변수가 1개이기 때문에 (props) => 에서 소괄호가 생략됐던 것
-// 그렇게 들어오는 props.$align 의 기본값을 설정해주기 위해서는
-// 소괄호를 생략하지 않고 (props) => 로 써줘야 되며,
-// 구조분해할당을 통해 ({ $align }) => 로 써줘야 함
-export const AdminButtonGroup = styled.div<{ $align?: "left" | "right" | "center" }>`
-    display: flex;
-    gap: 12px;
-    align-items: center;
-    justify-content: ${({ $align = "right" }) =>
-    $align === "right" ? "flex-end" : $align === "center" ? "center" : "flex-start"};
-`;
+    updateCategory,
+    fetchCategoryById,
+};
--- src/router/GetRouter.tsx
+++ src/router/GetRouter.tsx
@@ -8,6 +8,7 @@ import MainLayout from "../layouts/MainLayout.tsx";
 import SignInPage from "../pages/auth/signin/SignInPage.tsx";
 import SignUpPage from "../pages/auth/signup/SignUpPage.tsx";
 import AdminLayout from "../layouts/AdminLayout.tsx";
+import AdminCategoryEditPage from "../pages/admin/category/edit/AdminCategoryEditPage.tsx";
 
 // 회원의 권한에 따라 접근할 수 있는 주소를 판별하기 위해서
 // react-router 라이브러리에서는 "로더(loader)"라는 기능을 제공함
@@ -73,6 +74,7 @@ const router = createBrowserRouter([
                 children: [
                     { index: true, element: <AdminCategoryListPage /> },
                     { path: "create", element: <AdminCategoryCreatePage /> },
+                    { path: "edit/:id", element: <AdminCategoryEditPage />},
                 ],
             },
         ],

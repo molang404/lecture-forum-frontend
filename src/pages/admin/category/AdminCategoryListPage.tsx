cbf42af31a6f437709693a70e7ed7410ff4f4c47
molang404 <art104233@naver.com>
2026-05-20 12:33:31 +0900

    review start

--- src/pages/admin/category/AdminCategoryListPage.tsx
+++ src/pages/admin/category/AdminCategoryListPage.tsx
@@ -8,6 +8,11 @@ function AdminCategoryListPage() {
     const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
+        // useEffect 안에서 비동기 함수를 async - await 방법으로 사용할거라면
+        // 함수를 만들어서 감싸주고, 그걸 실행하도록 문법에 맞춰 적음
+        // 그리고 그 함수 실행 역시 비동기 함수에 대한 실행이기 때문에
+        // then(() => {})       아무것도 안 하는 then을 붙여줌
+
         const loadCategories = async () => {
             try {
                 const data = await adminCategoryApi.fetchCategoryList();

c268cff65b11d85ccacd150a904cb5a3667729cf
molang404 <art104233@naver.com>
2026-05-21 17:27:15 +0900

    AdminCategoryCreatePage 생성

--- src/api/admin/adminCategoryApi.ts
+++ src/api/admin/adminCategoryApi.ts
@@ -1,5 +1,6 @@
 import axiosInstance from "../axiosInstance.ts";
 import type { Category } from "../../types/category.type.ts";
+import type {AdminCreateCategoryInputType} from "../../schemas/admin/category/adminCreateCategorySchema.ts";
 
 const fetchCategoryList = async (): Promise<Category[]> => {
     const response = await axiosInstance.get("/admin/category/list");
@@ -7,9 +8,14 @@ const fetchCategoryList = async (): Promise<Category[]> => {
 };
 
 // 카테고리 등록 API
+const createCategory = async (data: AdminCreateCategoryInputType) => {
+    const response = await axiosInstance.post("/admin/category/create", data);
+    return response.data.data;
+}
 
 // 카테고리 수정 API
 
 export default {
     fetchCategoryList,
+    createCategory,
 };

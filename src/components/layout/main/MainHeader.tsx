e4643e42f8862684fa57e0a29f5aa81e9923be24
molang404 <art104233@naver.com>
2026-05-27 13:10:46 +0900

    MainHeader에 category 출력

--- src/components/layout/main/MainHeader.tsx
+++ src/components/layout/main/MainHeader.tsx
@@ -6,6 +6,9 @@ import { useThemeStore } from "../../../stores/theme/themeStore.ts";
 import { useAuthStore } from "../../../stores/auth/authStore.ts";
 import { FiSettings, FiUser } from "react-icons/fi";
 import { Role } from "../../../types/user.type.ts";
+import { useEffect, useState } from "react";
+import type { Category } from "../../../types/category.type.ts";
+import categoryApi from "../../../api/user/categoryApi.ts";
 
 const HeaderContainer = styled.header`
     height: 64px;
@@ -34,9 +37,28 @@ const Logo = styled(Link)`
     font-size: 24px;
     font-weight: 800;
     color: ${props => props.theme.colors.primary};
+    margin-right: 60px;
 `;
 
-const NavGroup = styled.nav`
+const Nav = styled.nav`
+    display: flex;
+    align-items: center;
+    gap: 40px;
+    flex: 1;
+`;
+
+const NavItem = styled(Link)`
+    font-size: 16px;
+    font-weight: 600;
+    color: ${props => props.theme.colors.text.default};
+    transition: all 0.3s;
+    
+    &:hover {
+        color: ${props => props.theme.colors.primary};
+    }
+`;
+
+const NavGroup = styled.div`
     display: flex;
     align-items: center;
     gap: 16px;
@@ -46,6 +68,21 @@ function MainHeader() {
     const { theme, onChangeTheme } = useThemeStore();
     const { user, isLoggedIn, logout } = useAuthStore();
 
+    const [list, setList] = useState<Category[]>([]);
+
+    useEffect(() => {
+        const loadList = async () => {
+            try {
+                const data = await categoryApi.fetchCategoryList();
+                setList(data);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        loadList().then(() => {});
+    }, []);
+
     return (
         <HeaderContainer>
             <HeaderInner>
@@ -53,6 +90,15 @@ function MainHeader() {
                     <IoChatbubbles size={28} />
                     <span>토론대난투</span>
                 </Logo>
+
+                <Nav>
+                    {list.map(item => (
+                        <NavItem key={item.id} to={`/category/${item.id}`}>
+                            {item.name}
+                        </NavItem>
+                    ))}
+                </Nav>
+
                 <NavGroup>
                     <Button color={"primary"} variant={"icon"} onClick={onChangeTheme}>
                         {theme === "light" ? <IoSunny size={20} /> : <IoMoon size={20} />}

8722c8afe9697cd2311520003923daee54b4402e
molang404 <art104233@naver.com>
2026-06-02 09:05:55 +0900

    PostDetailPage 투표 기능 작성

--- src/types/post.type.ts
+++ src/types/post.type.ts
@@ -10,7 +10,18 @@ export interface Post {
     views: number;
     userId: number;
     categoryId: number;
+    option1Text: string | null;
+    option2Text: string | null;
     // Generic Type 중 Pick 이라는 타입이 존재
     // Pick<해당 타입, 가져올 항목들>
     user: Pick<User, "id" | "nickname" | "email">;
+    // vote라는 프로퍼티(항목)을
+    // 글 내용에서만 있고, 글 목록에서는 없는 항목
+    // 그렇기 때문에 union을 줘서 null 일 수 있음을 고지
+    vote: {
+        option1Count: number;
+        option2Count: number;
+        totalCount: number;
+        hasVoted: boolean;
+    } | null;
 }
